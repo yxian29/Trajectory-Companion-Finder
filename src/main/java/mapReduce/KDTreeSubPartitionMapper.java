6b93f194a88a227178b7292b9859cc35dcf12afe
samsonxian <samsonxian729@gmail.com>
2016-01-19 20:56:16 -0500

    dev commit

--- src/main/java/mapReduce/KDTreeSubPartitionMapper.java
+++ src/main/java/mapReduce/KDTreeSubPartitionMapper.java
@@ -1,7 +1,11 @@
 package mapReduce;
 
+import Utils.MathUtil;
 import geometry.TCPoint;
 import geometry.TCRegion;
+import org.apache.commons.collections.IteratorUtils;
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.math.util.MathUtils;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import scala.Tuple2;
 import scala.Tuple3;
@@ -26,15 +30,30 @@ public class KDTreeSubPartitionMapper implements
     @Override
     public Iterable<Tuple3<Integer, Integer, TCRegion>> call(Tuple2<Integer, Iterable<TCPoint>> slot) throws Exception {
 
-        List<Tuple3<Integer, Integer, TCRegion>> regions = new ArrayList<>();
+        List<Tuple3<Integer, Integer, TCRegion>> regions = new ArrayList<>(_numSubPartitions);
 
         int m = 1;
         while(m < _numSubPartitions)
         {
+            double varX = getVariance(slot._2(), true);
+            double varY = getVariance(slot._2(), false);
+            //to-do
             m += 1;
         }
 
-
         return regions;
+    }
+
+    public double getVariance(Iterable<TCPoint> points, boolean coordinateX)
+    {
+        List<Double> values = new ArrayList<>();
+        for(TCPoint p : points) {
+            if(coordinateX)
+                values.add(p.getX());
+            else
+                values.add(p.getY());
+        }
+        Double[] ds = values.toArray(new Double[values.size()]);
+        return MathUtil.variance(ArrayUtils.toPrimitive(ds));
     }
 }
